a21a7e5d5895eefbc025f471a9ae1b74c2e0c258
mclarke <mclarkestudio@gmail.com>
2023-11-19 00:17:12 -0500

    add CNAME, pages deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 ./build.sh && 
 COMMIT=$(git log -1 --pretty=%B) && 
 echo $COMMIT &&
-cp -a www/. ~/code/mclarkedev.github.io/ &&
 cd ~/code/mclarkedev.github.io/ &&
+rm -r ./* && 
+cp -a ~/code/website/www/. ~/code/mclarkedev.github.io/ &&
 git add . &&
 git commit -m "deploy: $COMMIT" &&
 git push
