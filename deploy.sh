25677f304909a9f7f29c2029dfe3620354b82256
mclarke <mclarkestudio@gmail.com>
2023-11-27 15:46:24 -0500

    update deploy www nuke

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 COMMIT=$(git rev-parse --short HEAD) && 
 echo $COMMIT &&
 cd ~/code/mclarkedev.github.io/ &&
-rm -r ./* &&
+git rm -rf . &&
 cp -a ~/code/website/www/. ~/code/mclarkedev.github.io/ &&
 git add . &&
 git commit -m "deploy: $COMMIT" &&
